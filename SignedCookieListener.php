0c049d10a81d16eb115999d66c612de0f48f847c
Igor Wiedler <igor@wiedler.ch>
2011-07-20 17:31:22 +0200

    Nelmio {AG => }

--- SignedCookieListener.php
+++ SignedCookieListener.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Nelmio SecurityBundle.
  *
- * (c) Nelmio AG <hello@nelm.io>
+ * (c) Nelmio <hello@nelm.io>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
