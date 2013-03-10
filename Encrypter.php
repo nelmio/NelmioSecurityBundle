d2415c63caf8208b3e34d4a6ab0188267d9895eb
Ruud Kamphuis <ruudk@mphuis.com>
2013-03-10 18:29:55 +0100

    Added mcrypt check to Encrypter

--- Encrypter.php
+++ Encrypter.php
@@ -22,8 +22,12 @@ class Encrypter
     {
         $this->secret = substr($secret, 0, 32);
         $this->algorithm = $algorithm;
+        
+        if (!function_exists('mcrypt_module_open')) {
+            throw new \RuntimeException('You need to install mcrypt if you want to encrypt your cookies.');
+        }
 
-        $this->module = mcrypt_module_open($this->algorithm, '', MCRYPT_MODE_CBC, '');
+        $this->module = @mcrypt_module_open($this->algorithm, '', MCRYPT_MODE_CBC, '');
         if ($this->module === false) {
             throw new \InvalidArgumentException(sprintf("The supplied encryption algorithm '%s' is not supported by this system.",
                 $this->algorithm));
