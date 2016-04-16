3712972aa391e7271faa253a50b7d15fb9e3d417
Romain Neutron <imprec@gmail.com>
2016-04-16 16:37:57 +0200

    Drop Content-Security-Policy legacy configuration

--- Tests/DependencyInjection/ConfigurationTest.php
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -9,34 +9,6 @@ use Symfony\Component\Yaml\Parser;
 
 class ConfigurationTest extends \PHPUnit_Framework_TestCase
 {
-    public function testCspReportOnly()
-    {
-        $this->processYamlConfiguration(
-            "csp:\n" .
-            "  script:\n" .
-            "    - 'self'\n" .
-            "  report_only: true"
-        );
-    }
-
-    public function testCspReportOnlyDefaultIsFalse()
-    {
-        $config = $this->processYamlConfiguration(
-            "csp: ~"
-        );
-        $this->assertArrayHasKey('report_only', $config['csp']);
-        $this->assertFalse($config['csp']['report_only'], 'default value for report_only config should be false');
-    }
-
-    public function testCspReportOnlyConfigIsRespectedIfPresent()
-    {
-        $config = $this->processYamlConfiguration(
-            "csp:\n" .
-            "  report_only: true"
-        );
-        $this->assertTrue($config['csp']['report_only']);
-    }
-
     public function testCspWithReportAndEnforceSubtrees()
     {
         $this->processYamlConfiguration(
@@ -75,36 +47,6 @@ class ConfigurationTest extends \PHPUnit_Framework_TestCase
             "      - 'self'\n" .
             "    upgrade-insecure-requests:\n" .
             "      - 'self'\n"
-        );
-    }
-
-    /**
-     * @expectedException Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
-     * @expectedExceptionMessage report_only" and "(report|enforce)" can not be used together
-     */
-    public function testCspExclusiveReportOnlyAndEnforceSubtree()
-    {
-        $this->processYamlConfiguration(
-            "csp:\n" .
-            "  report_only:\n" .
-            "  enforce:\n" .
-            "    script-src:\n" .
-            "      - 'self'"
-        );
-    }
-
-    /**
-     * @expectedException Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
-     * @expectedExceptionMessage report_only" and "(report|enforce)" can not be used together
-     */
-    public function testCspExclusiveReportOnlyAndReportSubtree()
-    {
-        $this->processYamlConfiguration(
-            "csp:\n" .
-            "  report_only:\n" .
-            "  report:\n" .
-            "    script-src:\n" .
-            "      - 'self'"
         );
     }
 
