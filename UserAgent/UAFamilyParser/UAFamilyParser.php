bc0b770d1e06c0c750209b923b27af8ac964c4e2
Karoly Marton <karoly@collect.org>
2020-05-01 21:00:43 +0200

    Adding missing type conversion as parse requires string. (fatal error was thrown on missing user-agent header)

--- UserAgent/UAFamilyParser/UAFamilyParser.php
+++ UserAgent/UAFamilyParser/UAFamilyParser.php
@@ -24,6 +24,7 @@ class UAFamilyParser implements UAFamilyParserInterface
 
     public function getUaFamily($userAgent)
     {
+        if ($userAgent === null) $userAgent = '';
         return strtolower($this->parser->parse($userAgent)->ua->family);
     }
 }
