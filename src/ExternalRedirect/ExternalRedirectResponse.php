2e021f2043c6f24228a149dbf8506cfee99e069d
Martijn Croonen <martijn@inventis.be>
2023-12-09 13:24:17 +0100

    Apply coding standards
    Ran PHP CS autofixer.

--- src/ExternalRedirect/ExternalRedirectResponse.php
+++ src/ExternalRedirect/ExternalRedirectResponse.php
@@ -1,5 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the Nelmio SecurityBundle.
+ *
+ * (c) Nelmio <hello@nelm.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Nelmio\SecurityBundle\ExternalRedirect;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
