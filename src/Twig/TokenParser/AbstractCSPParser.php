0b812014faf2859ec870f5f3e418c36a48f8edd2
Jordi Boggiano <j.boggiano@seld.be>
2025-03-11 11:05:57 +0100

    Fix CI some more

--- src/Twig/TokenParser/AbstractCSPParser.php
+++ src/Twig/TokenParser/AbstractCSPParser.php
@@ -42,7 +42,7 @@ abstract class AbstractCSPParser extends AbstractTokenParser
 
         $sha = null;
         if ($body instanceof TextNode) {
-            assert(is_string($body->getAttribute('data')));
+            \assert(\is_string($body->getAttribute('data')));
             $sha = $this->computeSha($body->getAttribute('data'));
         }
 
