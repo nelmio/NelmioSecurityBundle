0d5e4f0c186846784ea08b5231ecbf43ab1de6ae
Silas Joisten <silasjoisten@proton.me>
2025-09-18 19:14:19 +0300

    Fix

--- tests/Functional/PermissionsPolicyTest.php
+++ tests/Functional/PermissionsPolicyTest.php
@@ -23,6 +23,6 @@ final class PermissionsPolicyTest extends WebTestCase
 
         $client->request('GET', '/');
 
-        $this->assertResponseHeaderSame('permissions-policy', 'camera=(), microphone=(self), geolocation=(*), fullscreen=(self "https://trusted-domain.com"), interest-cohort=(), accelerometer=(self), ambient-light-sensor=(self), attribution-reporting=(*), autoplay=(self), bluetooth=(self), browsing-topics=(*), captured-surface-control=(self), compute-pressure=(self), cross-origin-isolated=(self), deferred-fetch=(self), deferred-fetch-minimal=(*), display-capture=(self), encrypted-media=(self), gamepad=(self), gyroscope=(self), hid=(self), identity-credentials-get=(self), idle-detection=(self), language-detector=(self), local-fonts=(self), magnetometer=(self), midi=(self), otp-credentials=(self), payment=(self), picture-in-picture=(*), publickey-credentials-create=(self), publickey-credentials-get=(self), screen-wake-lock=(self), serial=(self), speaker-selection=(self), storage-access=(*), summarizer=(self), translator=(self), usb=(self), web-share=(self), window-management=(self), xr-spatial-tracking=(self)');
+        $this->assertResponseHeaderSame('permissions-policy', 'camera=(), microphone=(self), geolocation=(*), fullscreen=(self "https://trusted-domain.com"), interest-cohort=()');
     }
 }
